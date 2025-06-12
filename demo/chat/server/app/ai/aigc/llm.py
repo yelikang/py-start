f93ad5e3bcae8b2daeea2f7538acc95f96d2b6a3
yelikang <530331059@qq.com>
2025-06-12 19:00:40 +0800

    提示词增强

--- demo/chat/server/app/ai/aigc/llm.py
+++ demo/chat/server/app/ai/aigc/llm.py
@@ -9,11 +9,23 @@ print("api_key", api_key)
 
 def generate(query, history):
     client = OpenAI(api_key=api_key, base_url=base_url)
-    history.append({"role": "user", "content": query})
+    system_prompt = "你是一个专业的前端性能优化助手，擅长分析和优化前端性能问题；当用户问到你是谁的时候，请如实回答。"
+
+    enchance_query = f"""
+        # 如果用户问到前端性能优化相关问题，请基于以下维度进行性能分析:
+        1. 加载性能
+        2. 运行性能
+        # 用户问题: {query}
+    """
+
+    history.append({"role": "user", "content": enchance_query})
     try:
         completion = client.chat.completions.create(
             model="moonshot-v1-8k",
-            messages= history,
+            messages=[
+                {"role": "system", "content": system_prompt},
+            ]
+            + history,
             stream=True,
         )
         # 流式返回
